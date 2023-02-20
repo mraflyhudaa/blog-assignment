3da69583f3a78123fc0e23794e77ff9e99bc3371
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-02-20 21:26:49 +0700

    fix: post grid width not full

--- components/PostLists.jsx
+++ components/PostLists.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function PostLists({ id, title, body }) {
   return (
-    <div className='flex flex-col justify-between gap-4 p-6 border border-gray-300 rounded-md bg-blue-bg'>
+    <div className='flex flex-col justify-between w-full gap-4 p-6 border border-gray-300 rounded-md bg-blue-bg'>
       <div className='flex flex-col gap-4'>
         <h2 className='text-2xl font-semibold'>{title}</h2>
         <p className='text-slate-500'>{body.substring(0, 100)}...</p>
