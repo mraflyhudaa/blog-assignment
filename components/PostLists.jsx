6adf11ecd566a9de0e92b6b01d1a9f38e108e77f
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-02-20 20:59:29 +0700

    feat: add post lists page

---
+++ components/PostLists.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function PostLists({ id, title, body }) {
+  return (
+    <div className='flex flex-col justify-between gap-4 p-6 border border-gray-300 rounded-md bg-blue-bg'>
+      <div className='flex flex-col gap-4'>
+        <h2 className='text-2xl font-semibold'>{title}</h2>
+        <p className='text-slate-500'>{body.substring(0, 100)}...</p>
+      </div>
+      <Link
+        href={`/posts/${id}`}
+        className='p-2 ml-auto text-center text-white transition-all rounded-md bg-primary hover:bg-primary-hover disabled:bg-slate-400 disabled:cursor-not-allowed'
+      >
+        Read More
+      </Link>
+    </div>
+  );
+}
---
+++ pages/posts/index.jsx
@@ -0,0 +1,93 @@
+import instance from '@/api/axiosConfig';
+import { getAllPosts } from '@/api/posts';
+import PostLists from '@/components/PostLists';
+import Spinner from '@/components/Spinner';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+
+export default function Posts() {
+  const [page, setPage] = useState(1);
+
+  const {
+    isLoading,
+    isError,
+    data: posts,
+    isSuccess,
+    error,
+    isFetching,
+    isPreviousData,
+  } = useQuery({
+    queryKey: ['posts', page],
+    queryFn: () => getAllPosts(page),
+    keepPreviousData: true,
+  });
+
+  const getPosts = (page) => {
+    return instance
+      .get(`/posts?page=${page}&per_page=10`)
+      .then((res) => res)
+      .catch((error) => error);
+  };
+
+  console.log(getPosts(1));
+
+  if (isLoading || isFetching) {
+    return (
+      <div className='flex items-center justify-center w-full h-screen'>
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className='flex items-center justify-center w-full h-screen'>
+        Error {error.message}
+      </div>
+    );
+  }
+
+  if (isSuccess) {
+    return (
+      <div className='flex flex-col'>
+        <div className='grid grid-cols-1 gap-12 mx-auto mt-16 lg:grid-cols-2'>
+          {posts?.map((post) => (
+            <div key={post.id} className='flex w-full'>
+              <PostLists
+                id={post.id}
+                title={post.title}
+                userId={post.user_id}
+                body={post.body}
+              />
+            </div>
+          ))}
+        </div>
+        <div className='flex flex-col items-center justify-center gap-2 mt-6'>
+          <span>Current Page: {page}</span>
+          <div className='flex gap-4'>
+            <button
+              className='p-2 text-white transition-all rounded-md bg-primary hover:bg-primary-hover disabled:bg-slate-400 disabled:cursor-not-allowed'
+              onClick={() => setPage((old) => Math.max(old - 1, 0))}
+              disabled={page === 1}
+            >
+              Previous Page
+            </button>
+            <button
+              className='px-4 py-2 text-white transition-all rounded-md bg-primary hover:bg-primary-hover disabled:bg-slate-400 disabled:cursor-not-allowed'
+              onClick={() => {
+                if (!isPreviousData) {
+                  setPage((old) => old + 1);
+                }
+              }}
+              disabled={isPreviousData}
+            >
+              Next Page
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <></>;
+}
