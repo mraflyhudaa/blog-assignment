67659ccfa32a145950905f3053474fabf59c6a2a
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-02-20 20:49:06 +0700

    style: extend custom color scheme and custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
@@ -9,7 +11,25 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-bg': '#fffffe',
+        'blue-bg': '#d8eefe',
+        headline: '#094067',
+        paragraph: '#5f6c7b',
+        primary: '#3da9fc',
+        'primary-hover': '#348dd1',
+        secondary: '#90b4ce',
+        tertiary: '#ef4565',
+        'tertiary-hover': '#c73a54',
+        'button-text': '#fffffe',
+        stroke: '#094067',
+        main: '#fffffe',
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
