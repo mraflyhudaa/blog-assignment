da934056612c05ef5364a1f87c6c0158fcb6f9da
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-02-21 22:20:22 +0700

    fix: access token undefined

--- api/axiosConfig.js
+++ api/axiosConfig.js
@@ -5,7 +5,7 @@ const instance = axios.create({
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
     'X-Pagination-Pages': 50,
   },
 });
