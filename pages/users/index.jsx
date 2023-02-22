e6cc3a59ea6e070dfab1294143404497efffc96b
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-02-22 15:05:08 +0700

    fix: search bar width overflow

--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -60,8 +60,8 @@ export default function Users() {
   return (
     <div className='z-0 flex flex-col justify-center gap-4 mt-14'>
       <h1 className='text-3xl font-semibold text-headline'>Users</h1>
-      <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
-        <div className='flex flex-col-reverse justify-between gap-4 p-4 lg:flex-row bg-primary'>
+      <div className='rounded-lg shadow-md'>
+        <div className='flex flex-col-reverse justify-between gap-4 p-4 rounded-t-md sm:flex-row bg-primary'>
           <div>
             <label htmlFor='table-search' className='sr-only'>
               Search
@@ -84,7 +84,7 @@ export default function Users() {
               </div>
               <input
                 type='text'
-                className='block p-2 pl-10 text-sm border border-gray-200 rounded-lg text-headline w-80 bg-gray-50 focus:ring-primary focus:border-primary '
+                className='block w-full p-2 pl-10 text-sm border border-gray-200 rounded-lg text-headline bg-gray-50 focus:ring-primary focus:border-primary'
                 placeholder='Search for items'
                 onChange={({ target: { value } }) => setSearchValue(value)}
                 value={searchValue}
@@ -93,20 +93,22 @@ export default function Users() {
           </div>
           <button
             type='button'
-            className='px-4 py-2 text-sm leading-tight text-white transition-all bg-green-600 rounded-lg hover:bg-green-700 disabled:bg-slate-400 disabled:cursor-not-allowed'
+            className='w-auto px-4 py-2 text-sm leading-tight text-white transition-all bg-green-600 rounded-lg hover:bg-green-700 disabled:bg-slate-400 disabled:cursor-not-allowed'
             onClick={() => setIsOpen(!isOpen)}
           >
             Create User
           </button>
         </div>
-        {isOpen ? (
-          <UserModal
-            setIsOpen={setIsOpen}
-            page={page}
-            debouncedSearchValue={debouncedSearchValue}
-          />
-        ) : null}
-        {renderResults()}
+        <div className='relative overflow-x-auto '>
+          {isOpen ? (
+            <UserModal
+              setIsOpen={setIsOpen}
+              page={page}
+              debouncedSearchValue={debouncedSearchValue}
+            />
+          ) : null}
+          {renderResults()}
+        </div>
       </div>
     </div>
   );
